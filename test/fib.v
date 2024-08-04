8109c03adc797d341677fa12509d245612269382
tokyo4j <hrak1529@gmail.com>
2024-08-05 00:52:06 +0900

    Fixed fib.v

--- test/fib.v
+++ test/fib.v
@@ -8,32 +8,32 @@
 `timescale 1ns/10ps
 
 `define MEM_FILE_NAME "test/fib.dat"
-`include "test/exmemory.v"
+`include "sram.v"
 
 /* top level design for testing */
 module top;
   reg clk, rst;
-  wire w_en;
+  wire w_en, breq;
   wire [31:0] i;
   wire [7:0] r, w, i_addr, rw_addr;
   // 10nsec --> 100MHz
   parameter STEP = 10.0;
   // instantiate devices to be tested
-  mips dut(i_addr, i,
-           rw_addr, r, w, w_en,
-           clk, rst);
+  mips mips0(i_addr, i,
+             rw_addr, r, w, w_en, breq,
+             clk, rst);
   // external memory for code and data
-  exmemory exmem(i_addr, i,
-                 rw_addr, r, w, w_en,
-                 clk);
+  sram sram0(i_addr, i,
+             rw_addr, r, w, w_en,
+             clk);
   // initialize test
   initial begin
 `ifdef __POST_PR__
-    $sdf_annotate("mips.sdf", top.dut, , "sdf.log", "MAXIMUM");
+    $sdf_annotate("mips.sdf", top.mips0, , "sdf.log", "MAXIMUM");
 `endif
     // dump waveform
     $dumpfile("dump.vcd");
-    $dumpvars(0, top.dut);
+    $dumpvars(0, top.mips0);
     // rst
     clk <= 0; rst <= 1; # 22; rst <= 0;
     // stop at 1,000 cycles
